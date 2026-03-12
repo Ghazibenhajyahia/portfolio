f75a35ba92982b82fa86bb4e93419573f86c9912
Ghazibenhajyahia <78875896+Ghazibenhajyahia@users.noreply.github.com>
2026-03-12 03:58:25 +0100

    fix: multi-page vite config + audio resume

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
 export default {
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blackhole: resolve(__dirname, 'blackhole.html'),
+      }
+    }
+  }
 }
